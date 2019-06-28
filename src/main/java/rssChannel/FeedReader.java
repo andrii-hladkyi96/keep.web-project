a3da6de925941be0e4169c19e2c47e933097e16b
Andrii Hladkyi <andriihladkyi96@gmail.com>
2019-06-28 16:52:38 +0300

    added RSS reader

---
+++ src/main/java/rssChannel/FeedReader.java
@@ -0,0 +1,49 @@
+package rssChannel;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+public class FeedReader {
+private static String FirstURL = "http://report.if.ua/feed/";
+/*Вам потрібно переглянути всі URL-адреси на своєму веб-сайті, а потім знайти файл, який містить "rss".
+
+Метод вище, можливо, не буде працювати в деяких випадках, якщо URL-адресу в тезі href виглядає приблизно як feed.xml ,
+тому в цьому випадку вам потрібно буде пропустити все теги, що містять href І rss, а потім просто проаналізувати URL-адресу
+з атрибута href.
+
+Якщо ви хочете зробити це через браузер, натисніть CTRL + U, щоб переглянути джерело, потім CTRL + F, щоб відкрити вікно
+пошуку, а потім просто введіть rss . URL-адресу RSS-каналу повинен з'явитися негайно.
+*/
+
+private static String xmlFile;
+    public static void main(String[] args) {
+        boolean ok = false;
+        try {
+            URL feedUrl = new URL(FirstURL);
+
+            SyndFeedInput input = new SyndFeedInput();
+            XmlReader xmlReader = new XmlReader(feedUrl);
+            SyndFeed feed = input.build(xmlReader);
+
+            System.out.println(feed);
+            ok = true;
+        }
+        catch (Exception ex) {
+                ex.printStackTrace();
+                System.out.println("ERROR: "+ex.getMessage());
+        }
+
+        if (!ok) {
+            System.out.println();
+            System.out.println("FeedReader reads and prints any RSS/Atom feed type.");
+            System.out.println("The first parameter must be the URL of the feed to read.");
+            System.out.println();
+        }
+    }
+
+}
